c92cafe34831c2e24a5251e648caf743850f2475
Omar Khandoker <lockmail3435@gmail.com>
2026-02-12 10:03:15 +0600

    d2:10:03

--- controll/userControll.js
+++ controll/userControll.js
@@ -1,5 +1,6 @@
 import Connect from '../model/contactSchem.js'
 import mongoose from 'mongoose'
+import {validationResult} from 'express-validator'
 
 
 export const getAllData = async (req, res) => {
@@ -31,27 +32,97 @@ export const getAllData = async (req, res) => {
   
 }
 
-// export const showAllData = async (req, res) => {
-//   const getData = await Connect.find()
-//   res.json(getData)
-// }
-// export const addData = async (req, res) => {
-//   const getData = await Connect.find()
-//   res.json(getData)
-// }
-// export const addNewData = async (req, res) => {
-//   const getData = await Connect.find()
-//   res.json(getData)
-// }
-// export const updateData = async (req, res) => {
-//   const getData = await Connect.find()
-//   res.json(getData)
-// }
-// export const updateNewData = async (req, res) => {
-//   const getData = await Connect.find()
-//   res.json(getData)
-// }
-// export const deleteData = async (req, res) => {
-//   const getData = await Connect.find()
-//   res.json(getData)
-// }
+export const showAllData = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.render('404' , {message : 'Invalid Id'})
+  }
+  try {
+    const getData = await Connect.findById(req.params.id)
+    if (!getData) return res.render("404", { message: "User Not Found" });
+    return res.render('show' , {getData})
+  } catch (err) {
+     res.render("500", { message: err });
+  }
+}
+
+export const addData = async (req, res) => {
+  try {
+    await res.render('add')
+  } catch (err) {
+      res.render("500", { message: err });
+  }
+}
+
+export const addNewData = async (req, res) => {
+  try {
+    const error = validationResult(req);
+    if (error.isEmpty()) {
+      await Connect.create(req.body);
+      const { page = 1 ,limit = 5 } = req.query
+      
+      const option = {
+        page: parseInt(page),
+        limit : parseInt(limit)
+      }
+
+      const result = await Connect.paginate({}, option)
+
+      var a = result.totalPages;
+        res.redirect(`/?page=${a}`);
+    } else {
+      res.send(error)
+    }
+
+    
+  } catch(err) {
+      res.render("500", { message: err });
+  }
+
+}
+export const updateData = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const getData = await Connect.findById(req.params.id);
+    if(!getData) return res.render("404", { message: "User Not Found" });
+    res.render('update', {getData})
+  } catch (err) {
+     res.render("500", { message: err });
+  }
+}
+
+export const updateNewData = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+     return res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const error = validationResult(req)
+    if (error.isEmpty) {
+      const getData = await Connect.findByIdAndUpdate(req.params.id, req.body);
+      if (!getData) return res.render("404", { message: "User Not Found" });
+      res.redirect("/");
+    } else {
+      res.send(error);
+    }
+    
+  } catch (err) {
+    res.render("500", { message: err });
+  }
+  const getData = await Connect.find()
+  res.json(getData)
+}
+
+
+export const deleteData = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.render("404", { message: "Invalid Id" });
+  }
+  try {
+    const getData = await Connect.findByIdAndDelete(req.params.id);
+    if(!getData) return res.render('404' , {message : "Invalid Id"})
+    return res.redirect("/");
+  } catch (err) {
+    res.render("500", { message: err });
+  }
+}
--- route/router.js
+++ route/router.js
@@ -1,8 +1,43 @@
 import express from 'express'
 const router = express.Router()
-import { getAllData } from '../controll/userControll.js';
+import { getAllData, showAllData, addData, addNewData, updateData, updateNewData, deleteData } from '../controll/userControll.js';
+import {body} from 'express-validator'
+
+
+const validationRegistation = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("First Name is require")
+    .isLength({ min: 3 })
+    .withMessage("Minimum : 3 Charecter")
+    .matches(/^[a-zA-Z\s]+$/)
+    .withMessage("Only letter Are Allow"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("First Name is require")
+    .isEmail()
+    .withMessage("Only letter Are Allow"),
+  ,
+  body("phone")
+    .trim()
+    .notEmpty()
+    .withMessage("Phone is required")
+    .isLength({ min: 10, max: 15 })
+    .withMessage("Phone must be 10-15 digits")
+    .isNumeric()
+    .withMessage("Only numbers allowed"),
+
+];
 
 router.get("/", getAllData );
+router.get("/show/:id", showAllData );
+router.get("/add", addData );
+router.post("/add", validationRegistation , addNewData );
+router.get("/update/:id", updateData );
+router.post("/update/:id",validationRegistation, updateNewData);
+router.get("/delete/:id", deleteData);
 
 
 export default router
